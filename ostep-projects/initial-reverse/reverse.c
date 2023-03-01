c07d3ea86704392ec7ac743cf9f8f147729190e8
xavier-kong <kongxianchengxavier@gmail.com>
2023-03-01 09:00:38 +0800

    progress with reverse

--- ostep-projects/initial-reverse/reverse.c
+++ ostep-projects/initial-reverse/reverse.c
@@ -1,5 +1,14 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int checkIfSameFile(char *file1, char *file2) {
+    // if / in file name, split and take only last portion
+    // if / not in file name, it is the file name
+    // strcmp files
+    return 0;
+}
 
 int main(int argc, char **argv) {
     if (argc == 1) {
@@ -7,26 +16,45 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    if (argc == 2 || argc > 3) {
+    if (argc > 3) {
         fprintf(stderr, "usage: reverse <input> <output>\n");
-        exit(1);
-    }
-
-    if (argv[1] == argv[2]) {
-        fprintf(stderr, "Input and output file must differ\n");
         exit(1);
     }
 
     FILE *fp = fopen(argv[1], "r");
 
     if (fp == NULL) {
-        fprintf(stderr, "error: cannot open file %s", argv[1]);
+        fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
         exit(1);
     }
 
-    // count number of lines
-    // malloc array and add string to array
-    // read lines backwards and write to new file
+    int isSameFile = checkIfSameFile(argv[1], argv[2]);
+
+    if (isSameFile) {
+        fprintf(stderr, "reverse: input and output file must differ\n");
+        exit(1);
+    }
+
+    FILE *output = fopen(argv[2], "w");
+
+    if (output == NULL) {
+        fprintf(stderr, "reverse: cannot open file '%s'\n", argv[2]);
+        exit(1);
+    }
+
+    char *lines[sizeof(size_t)];
+    char line[sizeof(size_t)];
+    int i = 0;
+
+    while (fgets(line, sizeof(line), fp)) {
+        lines[i] = malloc(strlen(line) + 1);
+        strcpy(lines[i], line);
+        i++;
+    }
+
+    for (int j = i; j > -1; j--) {
+        fprintf(output, "%s\n", lines[j]);
+    }
 
     return 0;
 }
