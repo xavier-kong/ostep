db94458e3e2f92b7d13475f6cfedb2dee8bb37fc
xavier-kong <kongxianchengxavier@gmail.com>
2023-03-01 21:08:50 +0800

    completed reverse.c

--- ostep-projects/initial-reverse/reverse.c
+++ ostep-projects/initial-reverse/reverse.c
@@ -1,12 +1,52 @@
+#include <libgen.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+char* concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1) + strlen(s2) + 1);
+
+    if (result == NULL) {
+        printf("malloc failed\n");
+        exit(1);
+    }
+
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+char *get_filename_ext(const char *filename) {
+    char *dot = strrchr(filename, '.');
+    if (!dot || dot == filename)
+        return "";
+    return dot + 1;
+}
+
+char *getFileName(char *path) {
+    char *bname;
+    char *ext;
+    char *path2 = strdup(path);
+    bname = basename(path2);
+    ext = get_filename_ext(path);
+    char *fullName = concat(bname, ext);
+    return fullName;
+}
+
 int checkIfSameFile(char *file1, char *file2) {
     // if / in file name, split and take only last portion
     // if / not in file name, it is the file name
     // strcmp files
+    // https://linux.die.net/man/3/basename
+    // https://stackoverflow.com/questions/5901624/how-to-extract-a-file-name-from-a-full-path
+    char *file1Name = getFileName(file1);
+    char *file2Name = getFileName(file2);
+
+    if (strcmp(file1Name, file2Name) == 0) { // strings are equal
+        return 1;
+    }
     return 0;
 }
 
@@ -52,8 +92,8 @@ int main(int argc, char **argv) {
         i++;
     }
 
-    for (int j = i; j > -1; j--) {
-        fprintf(output, "%s\n", lines[j]);
+    for (int j = i - 1; j > -1; j--) {
+        fprintf(output, "%s", lines[j]);
     }
 
     return 0;
